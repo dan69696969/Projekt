d13eeff7a449cbb01b3b00503501a163625016df
dan69696969 <macoun.daniel@seznam.cz>
2025-08-21 08:32:34 +0200

    new
    asasas

--- Assets/Scripts/QuitGame.cs
+++ Assets/Scripts/QuitGame.cs
@@ -1,10 +1,38 @@
 using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
 
 public class QuitGame : MonoBehaviour
 {
+    public GameObject hiddenText; // pøipoj v inspectoru
+    private int clickCount = 0;
+    public float quitDelay = 2f; // zpoždìní v sekundách
+
     public void OnExitClick()
     {
-        Debug.Log("Kliknuto na tlaèítko pro ukonèení!");
+        clickCount++;
+
+        if (clickCount == 1)
+        {
+            Debug.Log("První kliknutí – hra se ukonèí za chvilku...");
+            StartCoroutine(QuitAfterDelay());
+        }
+        else
+        {
+            Debug.Log("Více kliknutí – zobrazím skrytý text!");
+            if (hiddenText != null)
+                hiddenText.SetActive(true);
+        }
+    }
+
+    private IEnumerator QuitAfterDelay()
+    {
+        yield return new WaitForSeconds(quitDelay);
         Application.Quit();
+
+        // pro testování v Editoru
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
     }
 }
